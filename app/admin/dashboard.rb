ba154958f4ad23456b9fdbb331b0ffda72891473
David Moulton <dave@themoultons.net>
2012-09-25 14:11:10 -0600

    made dashboard for active admin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,14 +1,30 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
+  menu :priority => 1, :label => "Standings"
 
-  content :title => proc{ I18n.t("active_admin.dashboard") } do
-    div :class => "blank_slate_container", :id => "dashboard_default_message" do
-      span :class => "blank_slate" do
-        span "Welcome to Active Admin. This is the default dashboard page."
-        small "To add dashboard sections, checkout 'app/admin/dashboards.rb'"
+  content :title => "Standings" do
+    League.order("id desc").all.each do |league|
+      columns do
+
+        league.divisions.each do |division|
+          column do
+            panel "#{league.name} - #{division.name}" do
+              table_for division.teams.order("wins desc") do
+                column :name
+                column :wins
+                column :losses
+              end
+            end
+          end
+        end
       end
     end
+    #div :class => "blank_slate_container", :id => "dashboard_default_message" do
+    #  span :class => "blank_slate" do
+    #    span "Welcome to Active Admin. This is the default dashboard page."
+    #    small "To add dashboard sections, checkout 'app/admin/dashboards.rb'"
+    #  end
+    #end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
